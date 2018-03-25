a08c7afff313e829e3fe551a7d3a0ff39bcebf96
Amanda M <amandaseagull3@gmail.com>
2018-03-25 18:27:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,6 @@ class Dog
     @name = first_name
   end
  
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
   def name
     "#{@first_name} #{@last_name}".strip
   end
