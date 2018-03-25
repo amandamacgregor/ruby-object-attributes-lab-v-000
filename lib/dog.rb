daa01eda668be57b53b1aa03402e11e03f2ce13f
Amanda M <amandaseagull3@gmail.com>
2018-03-25 18:28:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
  
-  def initialize(last_name)
-    @name = first_name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
  
   def name
-    "#{@first_name} #{@last_name}".strip
+    @this_dogs_name
   end
- 
+  
 end
